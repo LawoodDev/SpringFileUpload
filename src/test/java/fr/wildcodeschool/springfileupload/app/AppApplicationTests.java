e3f9788f1ce99f99ced2a7bc9e13ef0a25a7b3ae
LawoodDev <erwann.sale96@gmail.com>
2020-01-15 09:32:53 +0100

    tss

---
+++ src/test/java/fr/wildcodeschool/springfileupload/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.springfileupload.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
